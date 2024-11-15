2540b0125014bfa7d92b0e41f6c723ea72746a0f
HRISHIKESH HARIRANG SHINDE <hshinde@royal-04.cs.wisc.edu>
2024-11-14 21:13:26 -0600

    TC9 fix set allocated mem to 0

--- xv6-public/trap.c
+++ xv6-public/trap.c
@@ -99,6 +99,7 @@ trap(struct trapframe *tf)
         cprintf("Could not allocate memory");
       }
       else {
+        memset(mem, 0, PGSIZE); //TC9 FAILED here.
         mappages(currproc->pgdir, (char *)va, PGSIZE, V2P(mem), PTE_W | PTE_U); //Check passed as void* ?
         currproc->info->n_loaded_pages[index]++;
         //If file mapping, read file:
@@ -107,7 +108,6 @@ trap(struct trapframe *tf)
           int fd = currproc->info->fd[index];
           struct file * open_file = currproc->ofile[fd];
           setoffset(open_file, offset);
-          cprintf("offset set:%d\n", offset);
           fileread(open_file, (char*)va, PGSIZE);
         }
         break;
